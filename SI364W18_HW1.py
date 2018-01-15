31057510a87e936e21cf054aa15906fb00586bcb
adambens <adambens@umich.edu>
2018-01-14 21:39:15 -0500

    Finished HW1

--- SI364W18_HW1.py
+++ SI364W18_HW1.py
@@ -13,6 +13,7 @@
 ## Below is code for one of the simplest possible Flask applications. Edit the code so that once you run this application locally and go to the URL 'http://localhost:5000/class', you see a page that says "Welcome to SI 364!"
 
 from flask import Flask
+from flask import request
 import requests
 import json
 
@@ -48,10 +49,7 @@ def problem_2(mname):
 	z = response.text
 	return z
 	
-# {
-#  "resultCount":0,
-#  "results": []
-# }
+
 
 ## [PROBLEM 3] - 250 points
 
@@ -60,29 +58,63 @@ def problem_2(mname):
 ## You can assume a user will always enter a number only.
 
 
-@app.route('/question')
+@app.route('/question', methods=["GET","POST"])
 def problem_3():
-	pass
+	formstring = """<br><br>
+    <form action="http://localhost:5000/question" method='POST'>
+    Enter Your Favorite Number: <br>
+<input type="text" name="favnum"> <br> 
+<input type="submit" value="Submit">
+"""
+	if request.method == 'POST':
+		favnumber = int(request.form['favnum'])
+		doublefavoritenum = favnumber * 2
+		new_str = "Double your number is " + str(doublefavoritenum)
+		return formstring + """<br><br>""" +  new_str
+	else:
+		return formstring + "failed"
+
 
 ## [PROBLEM 4] - 350 points
 
 ## Come up with your own interactive data exchange that you want to see happen dynamically in the Flask application, and build it into the above code for a Flask application, following a few requirements.
-
 ## You should create a form that appears at the route: http://localhost:5000/problem4form
-
-## Submitting the form should result in your seeing the results of the form on the same page.
-
+## Submitting the form should result in your seeing the results of the form on the same page. 
 ## What you do for this problem should:
 # - not be an exact repeat of something you did in class
 # - must include an HTML form with checkboxes and text entry
 # - should, on submission of data to the HTML form, show new data that depends upon the data entered into the submission form and is readable by humans (more readable than e.g. the data you got in Problem 2 of this HW). The new data should be gathered via API request or BeautifulSoup.
-
 # You should feel free to be creative and do something fun for you --
 # And use this opportunity to make sure you understand these steps: if you think going slowly and carefully writing out steps for a simpler data transaction, like Problem 1, will help build your understanding, you should definitely try that!
-
 # You can assume that a user will give you the type of input/response you expect in your form; you do not need to handle errors or user confusion. (e.g. if your form asks for a name, you can assume a user will type a reasonable name; if your form asks for a number, you can assume a user will type a reasonable number; if your form asks the user to select a checkbox, you can assume they will do that.)
-
 # Points will be assigned for each specification in the problem.
 
+@app.route('/problem4form', methods=["GET","POST"])
+def problem_4():
+	formstr = """<br><br>
+    <form action="http://localhost:5000/problem4form" method='POST'>
+    Enter Location for Coordinates (Ex: 'Ann Arbor') : <br>
+<input type="text" name="location"> <br> 
+<input type="submit" value="Submit">
+"""
+	googlebaseurl = "https://maps.googleapis.com/maps/api/geocode/json?"
+	googlekey = "AIzaSyBUyCIRN5GBTyznebbKTpeL6bJydO8NQGs"
+	gparams = {}
+	gparams['key'] = googlekey
+	if request.method == 'POST':
+		location = request.form['location']
+		gparams['address'] = location
+		googledata = requests.get(googlebaseurl, params = gparams)
+		googlelocation = json.loads(googledata.text)
+		lat = googlelocation["results"][0]["geometry"]["location"]["lat"]
+		lng = googlelocation["results"][0]["geometry"]["location"]["lng"]
+		x = "latitude: " + str(lat) + """<br>""" + "longitude" + str(lng)
+		return formstr + """<br><br>""" + x
+	else:
+		return formstr + "Enter Again"
+
+
+
+############################################################3
 if __name__ == '__main__':
     app.run()
